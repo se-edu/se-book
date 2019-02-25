0c06f9c83c4a79812a0834355c2327513bf9b394
marvinchin <chinkunsong.marvin@gmail.com>
2019-02-26 00:30:20 +0800

    Use Algolia plugin

--- _markbind/layouts/default/scripts.js
+++
@@ -1,9 +0,0 @@
-function initDocsearch() {
-  docsearch({
-    apiKey: '8310bb56d1ece8950bd20b0b8c080e10',
-    indexName: 'se-edu',
-    inputSelector: '#searchbar',
-    debug: false
-  });
-}
-MarkBind.afterSetup(initDocsearch);
